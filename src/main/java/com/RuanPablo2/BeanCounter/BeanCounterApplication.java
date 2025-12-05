50e25af65cfbf5681bc0ce8e05f3414cb59588e2
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2025-12-04 22:39:50 -0300

    Project created

---
+++ src/main/java/com/RuanPablo2/BeanCounter/BeanCounterApplication.java
@@ -0,0 +1,13 @@
+package com.RuanPablo2.BeanCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanCounterApplication.class, args);
+	}
+
+}
