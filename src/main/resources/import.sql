b3ad6f0acd5f1bf5ad04ddf3b668891ab3ea4ef1
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-03-17 23:29:43 -0300

    deploy ready

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,17 +1,17 @@
 -- USERS
-INSERT INTO users (name, email, password, created_at) VALUES ('Ruan Developer', 'ruan@dev.com', '$2a$10$W7YMyzA/rkHo61lr6u0r8eal8QEsGdm5HC/9jNoOD3qi/20pEjItS', CURRENT_TIMESTAMP);
-INSERT INTO users (name, email, password, created_at) VALUES ('Usuario Teste', 'teste@email.com', '$2a$10$W7YMyzA/rkHo61lr6u0r8eal8QEsGdm5HC/9jNoOD3qi/20pEjItS', CURRENT_TIMESTAMP);
+INSERT INTO users (name, email, password, created_at) VALUES ('admin', 'admn@dev.com', '$2a$10$W7YMyzA/rkHo61lr6u0r8eal8QEsGdm5HC/9jNoOD3qi/20pEjItS', CURRENT_TIMESTAMP);
+INSERT INTO users (name, email, password, created_at) VALUES ('Test user', 'test@email.com', '$2a$10$W7YMyzA/rkHo61lr6u0r8eal8QEsGdm5HC/9jNoOD3qi/20pEjItS', CURRENT_TIMESTAMP);
 
 -- INCOME
-INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Salário Mensal', 5000.00, '2025-01-05', 'INCOME', 1);
-INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Freelance Projeto Java', 1500.00, '2025-01-15', 'INCOME', 1);
+INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Monthly salary', 5000.00, '2025-01-05', 'INCOME', 1);
+INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Freelance Java Project', 1500.00, '2025-01-15', 'INCOME', 1);
 
 -- EXPENSE
-INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Aluguel', 1800.00, '2025-01-10', 'EXPENSE', 1);
-INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Supermercado', 400.00, '2025-01-12', 'EXPENSE', 1);
+INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Rent', 1800.00, '2025-01-10', 'EXPENSE', 1);
+INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Supermarket', 400.00, '2025-01-12', 'EXPENSE', 1);
 
 -- FEBRUARY - DATA FILTER TEST
-INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Conta de Luz', 150.00, '2025-02-10', 'EXPENSE', 1);
+INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Electricity Bill', 150.00, '2025-02-10', 'EXPENSE', 1);
 
 -- USER 2 TRANSACTIONS - To test data isolation
-INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Salário do Outro Cara', 10000.00, '2025-01-05', 'INCOME', 2);
+INSERT INTO transactions (description, amount, date, type, user_id) VALUES ('Other guy salary', 10000.00, '2025-01-05', 'INCOME', 2);
